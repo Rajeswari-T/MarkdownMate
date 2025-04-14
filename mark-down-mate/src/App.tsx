f14749cdb25a10618257e6338150aab22535bf2b
Rajeswari-T <talluridurgarajeswari@gmail.com>
2025-04-13 23:00:56 -0500

    create note added

---
+++ mark-down-mate/src/App.tsx
@@ -0,0 +1,87 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { NewNote } from "./NewNote";
+import { NoteList } from "./NoteList";
+import { useLocalStorage } from "./LocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidv4 } from "uuid";
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+export type Tag = {
+  id: string;
+  label: string;
+};
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        {
+          ...data,
+          id: uuidv4(),
+          tagIds: tags.map((tag) => tag.id),
+        },
+      ];
+    });
+  }
+  function onAddTag(newTag: Tag) {
+    setTags((prevTags) => [...prevTags, newTag]);
+  }
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <NoteList notes={notesWithTags} availableTags={tags}></NoteList>
+          }
+        />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={onAddTag}
+              availableTags={tags}
+            />
+          }
+        />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
---
+++ mark-down-mate/src/NoteList.tsx
@@ -0,0 +1,108 @@
+import { Col, Row, Stack, Button, Form, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { useState } from "react";
+import { Tag, Note } from "./App";
+import { useMemo } from "react";
+type NoteListProps = {
+  availableTags: Tag[];
+  notes: Note[];
+};
+type SimplifiedNote = {
+  title: string;
+  tags: Tag[];
+  id: string;
+};
+export function NoteList({ availableTags, notes }: NoteListProps) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
+  const filteredNotes = useMemo(() => {
+    return notes.filter((note) => {
+      return (
+        (title === "" ||
+          note.title.toLowerCase().includes(title.toLowerCase())) &&
+        (selectedTags.length === 0 ||
+          selectedTags.every((tag) =>
+            note.tags.some((noteTag) => noteTag.id === tag.id)
+          ))
+      );
+    });
+  }, [title, selectedTags, notes]);
+  return (
+    <>
+      <Row className="align-items-center mb-4">
+        <Col>
+          <h1> Notes </h1>
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return {
+                    label: tag.label,
+                    value: tag.id,
+                  };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return {
+                        label: tag.label,
+                        id: tag.value,
+                      };
+                    })
+                  );
+                }}
+                isMulti
+              ></ReactSelect>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+        {filteredNotes.map((note) => (
+          <Col key={note.id}>
+            <NoteCard
+              id={note.id}
+              title={note.title}
+              tags={note.tags}
+            ></NoteCard>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
+
+function NoteCard({ id, title, tags }: SimplifiedNote) {
+  return (
+    <Card>
+      <Card.Body></Card.Body>
+    </Card>
+  );
+}
